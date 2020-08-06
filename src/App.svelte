aa1959f63a7cc5acb6ddc146380fd2089afe25cb
Woo-sang yun <wsy04018@naver.com>
2020-08-06 18:33:55 +0900

    asdf

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+  let count = 0;
+  let check = 0;
+  let checktext = "on";
+
+  const Clickevent = () => {
+    count++;
+  };
+
+  const Checkchange = () => {
+    if (check == 1) {
+      check = 0;
+      checktext = "on";
+    } else {
+      check = 1;
+      checktext = "off";
+    }
+  };
+
+  const Longincrease = () => {
+    count += 30;
+  };
+
+  const Reset = () => {
+    count = 0;
+  };
+</script>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+</style>
+
+<main>
+  <p>{checktext}</p>
+  <p>{count}</p>
+  <button on:click={Clickevent}>click me</button>
+  <button on:click={Checkchange}>checkchange</button>
+  <button on:click={Longincrease}>+30</button>
+  <button on:click={Reset}>reset</button>
+</main>
